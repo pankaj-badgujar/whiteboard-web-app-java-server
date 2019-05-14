236861ef7be1b9438a61572d845b0657b9a94ffb
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-05-14 19:29:55 +0200

    creating spring boot application
    ran the spring init command

---
+++ src/test/java/com/example/cs5610summer2019javaserverpankajBadgujar/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.cs5610summer2019javaserverpankajBadgujar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
