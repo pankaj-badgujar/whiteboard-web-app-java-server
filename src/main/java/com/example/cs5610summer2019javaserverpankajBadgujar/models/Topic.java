9e9a9fd584aa8ff0d18fd28f5391aa87978f48b9
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-07-06 08:42:05 +0200

    created topics table

--- src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/models/Topic.java
+++ src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/models/Topic.java
@@ -2,15 +2,43 @@ package com.example.cs5610summer2019javaserverpankajBadgujar.models;
 
 import java.util.List;
 
+import javax.persistence.*;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="topics")
 public class Topic {
 
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	private String title;
-	private List<Widget> widgets;
-	public String getId() {
+	
+	@ManyToOne
+	@JsonIgnore
+	private Lesson lesson;
+	
+	public Topic() {
+		
+	}
+	
+	public Topic(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	
+	@Transient
+	public String getLessonTitle() {
+		return lesson.getTitle();
+	}
+	
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getTitle() {
@@ -19,10 +47,5 @@ public class Topic {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public List<Widget> getWidgets() {
-		return widgets;
-	}
-	public void setWidgets(List<Widget> widgets) {
-		this.widgets = widgets;
-	}
+	
 }
