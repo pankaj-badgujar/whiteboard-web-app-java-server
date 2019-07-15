077cb547996744950ae547e07dcc1d93820dbe7f
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-07-15 19:21:44 +0200

    added scanBasePackages

--- src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/DemoApplication.java
+++ src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.cs5610summer2019javaserverpankajBadgujar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.cs5610summer2019javaserverpankajBadgujar.controller"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
