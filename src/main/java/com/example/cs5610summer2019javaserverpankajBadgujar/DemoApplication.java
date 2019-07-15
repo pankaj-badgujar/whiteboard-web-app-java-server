76049119082d6996d7d15a1028ff046e848f67c5
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-07-15 19:27:18 +0200

    Update DemoApplication.java

--- src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/DemoApplication.java
+++ src/main/java/com/example/cs5610summer2019javaserverpankajBadgujar/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.cs5610summer2019javaserverpankajBadgujar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.cs5610summer2019javaserverpankajBadgujar.controller"})
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
